2baba00079657cc5b5624c7e5010bfaa70b06250
Ola Podsiadlik <alecs@op.pl>
2019-02-11 15:59:50 +0100

    Add task 18.7

--- components/contact.js
+++ components/contact.js
@@ -5,17 +5,13 @@ var Contact = React.createClass({
   
     render: function() {
       return (
-        React.createElement('div', {className: 'contactItem'},
-          React.createElement('img', {
-            className: 'contactImage',
-            src: 'http://chittagongit.com//images/contact-icon-png/contact-icon-png-5.jpg'
-          }),
-          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-            'e-mail: ' + this.props.item.email
-          )
+        <div className={'contactItem'}>
+          <img className={'contactImage'} src={'http://chittagongit.com//images/contact-icon-png/contact-icon-png-5.jpg'
+          }/>
+          <p className={'contactLabel'}>Imię: {this.props.item.firstName}</p>
+          <p className={'contactLabel'}>Nazwisko: {this.props.item.lastName}</p>
+          <a className={'contactEmail'} href={'mailto:' + this.props.item.email}>e-mail: {this.props.item.email}</a>
+        </div>  
         )
-      )
-    },
+      }
   });
